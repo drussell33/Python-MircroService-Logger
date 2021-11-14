bc45246d6741cc59d2962db84f4dd246965fb735
Derek Russell <drussell19@wou.edu>
2021-11-13 16:25:12 -0800

    removed old mogodb host

--- rundev.sh
+++ rundev.sh
@@ -4,6 +4,6 @@ export FLASK_ENV=development
 export DB='wolfit_activity_log'
 export DB_USER='admin-derek'
 export DB_PASSWORD='yXgR1K9tSXciFodd'
-export DB_HOST='mongodb+srv://admin-derek:yXgR1K9tSXciFodd@microservices407.pdlfu.mongodb.net/wolfit_activity_log?retryWrites=true&ssl_cert_reqs=CERT_NONE'
+export DB_HOST=''
 export SLEEP_TIME=50
 flask run --host=0.0.0.0 --port $@
